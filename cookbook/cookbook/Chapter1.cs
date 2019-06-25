2fc47c13b8015ba3816cbc08b0a28d230a9744a5
nbson <sonngb.hn@gmail.com>
2019-06-25 16:17:37 +0700

    chapter 1

---
+++ cookbook/cookbook/Chapter1.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Linq;
+
+namespace cookbook
+{
+    public class Chapter1
+    {
+        public (int average, int studentCount) GetAverageAndCount(int[] scores)
+        {
+            var returnTuple = (ave: 0, sCount: 0);
+
+            returnTuple = (scores.Sum() / scores.Count(), scores.Count());
+
+            return returnTuple;
+        }
+
+    }
+}
---
+++ cookbook/cookbook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace cookbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Chapter1 ch1 = new Chapter1();
+            var (average, studentCount) = ch1.GetAverageAndCount(scores);
+            WriteLine($"Average was {average} accross {studentCount} students");
+            ReadLine();
+        }
+    }
+}
