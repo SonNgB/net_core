ebbfc09137578cc3882ff84a73f1b4cef9bfc2b3
nbson <sonngb.hn@gmail.com>
2019-06-28 18:17:27 +0700

    update 4.0 Out variable

--- cookbook/cookbook/Chapter1.cs
+++ cookbook/cookbook/Chapter1.cs
@@ -1,19 +1,91 @@
 ﻿
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using static System.Console;
 
 namespace cookbook
 {
+
+    #region 0.2 tuples
+    //public class Chapter1
+    //{
+    //    public (double average, int studentCount, bool belowAverage) GetAverageAndCount(int[] scores, int threshold)
+    //    {
+    //        var returnTuple = (ave: 0D, sCount: 0, subAve: true);
+
+    //        returnTuple = ((double)scores.Sum() / scores.Count(), scores.Count(), returnTuple.ave.CheckIfBelowAverage(threshold));
+
+    //        return returnTuple;
+    //    }
+    //}
+
+    //public static class ExtensionMethods
+    //{
+    //    public static bool CheckIfBelowAverage(this double classAverage, int threshold)
+    //    {
+    //        if (classAverage < threshold)
+    //        {
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            return false;
+    //        }
+    //    }
+    //}
+    #endregion
+
+
+    #region 3.0 Pattern matching
+
     public class Chapter1
     {
-        public (int average, int studentCount) GetAverageAndCount(int[] scores)
+        public void OutputInfomation(object person)
         {
-            var returnTuple = (ave: 0, sCount: 0);
+            //if (person is Student student)
+            //{
+            //    WriteLine($"Student {student.Name} {student.LastName} is enrolled for coursr {String.Join<int>(" ", student.CourseCodes)}");
+            //}
 
-            returnTuple = (scores.Sum() / scores.Count(), scores.Count());
+            //if (person is Professor professor)
+            //{
+            //    WriteLine($"Professor {professor.Name} {professor.LastName} teaches {String.Join(" ", professor.TeacherSubject)}");
+            //}
 
-            return returnTuple;
+            switch (person)
+            {
+                case Student student:
+                    WriteLine($"Student {student.Name} {student.LastName} is enrolled for coursr {String.Join<int>(" ", student.CourseCodes)}");
+                    break;
+                case Professor professor:
+                    WriteLine($"Professor {professor.Name} {professor.LastName} teaches {String.Join(" ", professor.TeacherSubject)}");
+                    break;
+                case null:
+                    WriteLine($"Object {nameof(person)} is null");
+                    break;
+                default:
+                    WriteLine("Unknown object detected");
+                    break;
+            }
+
         }
-
     }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public List<int> CourseCodes { get; set; }
+    }
+
+    public class Professor
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public List<string> TeacherSubject { get; set; }
+    }
+    #endregion
+
 }
--- cookbook/cookbook/Program.cs
+++ cookbook/cookbook/Program.cs
@@ -1,17 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Console;
+using static System.Math;
 
 namespace cookbook
 {
     class Program
     {
+        #region 2.0 tuples
+        //static void Main(string[] args)
+        //{
+        //    int[] scores = { 17, 46, 39, 62, 81, 79, 52, 24, 49 };
+        //    Chapter1 ch1 = new Chapter1();
+        //    int threshold = 40;
+        //    var (average, studentCount, belowAverage) = ch1.GetAverageAndCount(scores, threshold);
+        //    WriteLine($"Average was {Round(average, 2)} accross {studentCount} students. {(average < threshold ? "1" : "2")}");
+        //    ReadLine();
+        //}
+        #endregion
+
+        #region 3.0 Pattern Matching
         static void Main(string[] args)
         {
-            int[] scores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Chapter1 ch1 = new Chapter1();
-            var (average, studentCount) = ch1.GetAverageAndCount(scores);
-            WriteLine($"Average was {average} accross {studentCount} students");
+            Student student = new Student();
+            student.Name = "John";
+            student.LastName = "Kirk";
+            student.CourseCodes = new List<int> { 203, 202, 101 };
+            ch1.OutputInfomation(student);
+            Professor professor = new Professor();
+            professor.Name = "Luke";
+            professor.LastName = "Lucky";
+            professor.TeacherSubject = new List<string> { "Math", "English" };
+            ch1.OutputInfomation(professor);
             ReadLine();
         }
+        #endregion
     }
 }
